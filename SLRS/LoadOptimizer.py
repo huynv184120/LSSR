8a37c5c2e844af776690c5c68fd7357239ef3187
huynv184120 <72036684+huynv184120@users.noreply.github.com>
2021-07-07 06:24:21 +0700

    Add files via upload

--- SLRS/LoadOptimizer.py
+++ SLRS/LoadOptimizer.py
@@ -1,4 +1,5 @@
 import os, sys
+from state.SavedState import SavedState
 from state.EdgeDemandState import EdgeDemandStateTree
 lib_path = os.path.abspath(os.path.join('core'))
 sys.path.append(lib_path)
@@ -6,7 +7,7 @@ from CapacityData import CapacityData
 
 lib_path = os.path.abspath(os.path.join('state'))
 sys.path.append(lib_path)
-from FlowState import FlowStateRecomputeDAG
+from FlowState import FlowStateRecomputeDAG , FlowStateRecomputeDAGOnCommit
 from EdgeDemandState import EdgeDemandStateTree
 from PathState import PathState
 
@@ -14,6 +15,12 @@ lib_path = os.path.abspath(os.path.join('constraint'))
 sys.path.append(lib_path)
 from MaxLoad import MaxLoad
 
+lib_path = os.path.abspath(os.path.join('neighborhood'))
+sys.path.append(lib_path)
+from Insert import Insert
+from Replace import Replace
+from Remove import Remove
+from Reset import Reset
 
 
 class LoadOptimizer:
@@ -24,10 +31,23 @@ class LoadOptimizer:
         self.nNodes = nNodes
         self.nEdges = nEdges
         self.decisionDemands = decisionDemands
+        self.edgeDemandState = EdgeDemandStateTree(self.nDemands, self.nEdges, self.capacity)
         self.pathState = PathState(decisionDemands)
         self.flowState = FlowStateRecomputeDAG(nNodes, nEdges, sp, self.pathState, decisionDemands)
+        self.flowStateOnCommit = FlowStateRecomputeDAGOnCommit(nNodes, nEdges, sp, self.pathState, decisionDemands, self.edgeDemandState)
         self.maxLoad = MaxLoad(nNodes, nEdges, capacity, self.flowState, sp)
-        self.edgeDemandState = EdgeDemandStateTree(self.nDemands, self.nEdges, self.capacity)
+        self.bestPaths = SavedState(self.pathState)
+
+        self.pathState.addTrial(self.flowState)
+        self.pathState.addTrial(self.flowStateOnCommit)
+        self.pathState.addTrial(self.maxLoad)
+        self.pathState.addTrial(self.bestPaths)
+
+        
+        neighborhoods = [Reset(self.pathState), Remove(self.pathState), Insert(self.nNodes, self.pathState), Replace(self.nNodes, self.pathState)]
+        kickNeighborhoods = [Reset(self.pathState), Remove(self.pathState)]
+
+
 
     def selectDemand(self):
         edge = self.maxLoad.selectRandomMaxEdge()
--- SLRS/state/FlowState.py
+++ SLRS/state/FlowState.py
@@ -76,6 +76,9 @@ class FlowStateRecomputeDAG(FlowStateChecker):
         self.initialize()
         self.commitState()
     
+    def check(self):
+        return True
+    
     def modify(self, src, dest, bw):
         assert src != dest
         paths = self.sp.pathEdges[src][dest]
@@ -84,6 +87,85 @@ class FlowStateRecomputeDAG(FlowStateChecker):
         for path in paths:
             for edge in path:
                 self.updateValue(edge, self.values[edge] + increment)
-
-
             
+
+
+class FlowStateRecomputeDAGOnCommit(ArrayStateFloat):
+    def __init__(self, nNodes, nEdges, pathState, demandsData, edgeDemandState):
+        super().__init__(nNodes)
+        self.nNodes = nNodes
+        self. nEdges = nEdges
+        self.pathState = pathState
+        self.demandsData = demandsData
+        self.edgeDemandState = edgeDemandState
+
+    def check(self):
+        return True
+    
+    def updateState(self):
+        pass
+
+    def commit(self):
+        self.updateFlowState()
+        super.commit()
+    
+    def initialize(self):
+        demand = self.pathState.nDemands
+        while demand > 0:
+            demand -= 1
+            path = self.pathState.path(demand)
+            pos = self.pathState.size(demand) - 1
+
+            while pos > 0:
+                pos -= 1
+                src = path[pos]
+                dest = path[pos+1]
+                self.modify(demand, src, dest, self.demandsData.demandTraffics[demand])    
+    
+
+    def updateFlowState(self):
+        pChanged = self.pathState.nChanged
+        changed = self.pathState.changed
+        while pChanged > 0:
+            pChanged -= 1
+            demand = changed[pChanged]
+            bandwidth = self.demandsData.demandTraffics[demand]
+
+            currentPath = self.pathState.path(demand)
+            currentSize = self.pathState.size(demand)
+            oldPath = self.pathState.oldPath(demand)
+            oldSize = self.pathState.oldSize(demand)
+
+            minSize = min(oldSize, currentSize)
+
+            firstDiff = 1
+
+            while firstDiff < minSize & currentPath[firstDiff] == oldPath[firstDiff]:
+                firstDiff += 1
+
+            endCurrent = currentSize - 2
+            endOld     = oldSize - 2
+
+            while (firstDiff < endCurrent) & (firstDiff < endOld) & (currentPath[endCurrent] == oldPath[endOld]):
+                endCurrent -= 1
+                endOld -=1
+
+            p = firstDiff - 1
+            while p <= endCurrent:
+                self.modify(demand, currentPath[p], currentPath[p+1], bandwidth)
+                p += 1
+            
+            q = firstDiff - 1
+            while q <= endOld:
+                self.modify(demand, oldPath[q], oldPath[q+1], -bandwidth)
+                q += 1
+
+    def modify(self, demand, src, dest, bw):
+        assert src != dest
+        paths = self.sp.pathEdges[src][dest]
+        nPaths = self.sp.nPaths[src][dest]
+        increment = bw / nPaths
+        for path in paths:
+            for edge in path:
+                self.updateValue(edge, self.values[edge] + increment)
+                self.edgeDemandState.updateEdgeDemand(edge, demand, increment)
